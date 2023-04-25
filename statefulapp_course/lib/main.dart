4b4b8305bfe5623df3c10e241114b93571a54d3f
Ciro <80367726+Cirorx@users.noreply.github.com>
2023-04-25 14:22:36 -0300

    Chapter 2 - InheritedWidget step 1

---
+++ statefulapp_course/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = "Tap the screen.";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: GestureDetector(
+          onTap: () {
+            setState(() {
+              title = DateTime.now().toIso8601String();
+            });
+          },
+          child: Container(
+            color: Colors.white,
+          )),
+    );
+  }
+}
