f3c002b5cd700732b59b89a8449cde791e9c9b76
Ciro <80367726+Cirorx@users.noreply.github.com>
2023-04-25 15:10:02 -0300

    Chapter 2 - InheritedWidget final

--- statefulapp_course/lib/main.dart
+++ statefulapp_course/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -7,7 +8,10 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: ApiProvider(
+        api: Api(),
+        child: const HomePage(),
+      ),
     ),
   );
 }
@@ -20,23 +24,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String title = "Tap the screen.";
+  ValueKey _textKey = const ValueKey<String?>(null);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(ApiProvider.of(context).api.dateAndTime ?? ''),
       ),
       body: GestureDetector(
-          onTap: () {
+          onTap: () async {
+            final api = ApiProvider.of(context).api;
+            final dateAndTime = await api.getDateAndTime();
             setState(() {
-              title = DateTime.now().toIso8601String();
+              _textKey = ValueKey(dateAndTime);
             });
           },
-          child: Container(
-            color: Colors.white,
+          child: SizedBox.expand(
+            child: Container(
+              color: Colors.white,
+              child: DateTimeWidget(
+                key: _textKey,
+              ),
+            ),
           )),
     );
+  }
+}
+
+class DateTimeWidget extends StatelessWidget {
+  const DateTimeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final api = ApiProvider.of(context).api;
+    return Text(
+      api.dateAndTime ?? "Tap on the screen to show date and time",
+    );
+  }
+}
+
+class Api {
+  //Just a fake api that will expose a function
+  String? dateAndTime;
+
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => DateTime.now().toIso8601String(),
+    ).then((value) {
+      dateAndTime = value;
+      return value;
+    });
+  }
+}
+
+class ApiProvider extends InheritedWidget {
+  final Api api;
+  final String uuid;
+
+  ApiProvider({
+    Key? key,
+    required this.api,
+    required Widget child,
+  })  : uuid = const Uuid().v4(),
+        super(
+          key: key,
+          child: child,
+        );
+
+  @override
+  bool updateShouldNotify(covariant ApiProvider oldWidget) {
+    //redraw or not
+    return uuid != oldWidget.uuid;
+  }
+
+  static ApiProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ApiProvider>()!;
   }
 }
